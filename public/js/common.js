c3e99392e9114fbfbe30e85cc48784a1f320d941
youzhen <yl301114@gmail.com>
2025-04-22 00:13:14 +0100

    finish

--- public/js/common.js
+++ public/js/common.js
@@ -2,3 +2,11 @@ $(".header_menu").click(function(){
     $(this).toggleClass("active");
     $(".header_nav").toggleClass("active");
 })
+
+$(window).on("resize scroll",function(){
+    if($(window).scrollTop()>0){
+        $(".header").addClass("scroll");
+    }else {
+        $(".header").removeClass("scroll");
+    }
+})
